6d0c80aed5316048bceacb82e3a41b90b2f68c21
Aivin Joy <aivinjohnjoy@gmail.com>
2024-09-25 11:01:54 +0530

    listurlupdate

--- mysite/module2/urls.py
+++ mysite/module2/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.print_hello),
+    path('', views.print_hello),
     path('details/', views.details),
     path ('add/', views.add),
     path ('edit/<pk>', views.edit),
